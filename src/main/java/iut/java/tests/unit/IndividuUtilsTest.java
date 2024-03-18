f3e42f12a98a9f497d0dcc910ce525e9430db1b2
Thomas Druineau <thomas.druineau@etu.unilim.fr>
2024-03-18 10:05:12 +0100

    Ajout testGetLine

---
+++ src/main/java/iut/java/tests/unit/IndividuUtilsTest.java
@@ -0,0 +1,36 @@
+package iut.java.tests.unit;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThat;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import iut.java.dto.IndividuDto;
+import iut.java.utils.IndividuUtils;
+
+public class IndividuUtilsTest {
+	
+	@Test
+	public void testGetLine() {
+		//ARRANGE
+		String expectedLine = new String("1,Billi,Beves,Ms,163,27/03/2001");
+		IndividuDto dto = new IndividuDto();
+		dto.setId((long) 1);
+	    dto.setFirstName("Billi");
+	    dto.setLastName("Beves");
+	    dto.setTitle("Ms");
+	    dto.setHeight(163);
+	    LocalDate birthDate = LocalDate.of(2001, 03, 27);
+        dto.setBirthDate(birthDate);
+
+		//ACT
+        String actualLine = IndividuUtils.getLine(dto);
+		//ASSERT
+        assertThat(actualLine).isEqualTo(expectedLine);
+        
+        
+	}
+}
